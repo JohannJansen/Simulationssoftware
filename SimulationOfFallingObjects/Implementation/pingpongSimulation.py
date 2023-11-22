0b657c9513cd6343efb45f769209543b86c7ce61
ada516 <jan.johannsen@haw-hamburg.de>
2023-11-22 21:54:48 +0100

    finished homework 4

--- SimulationOfFallingObjects/Implementation/pingpongSimulation.py
+++ SimulationOfFallingObjects/Implementation/pingpongSimulation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 #Constants
 IMPACTPOINTAPPROXIMATIONMAXDEPTH = 4
@@ -7,11 +8,11 @@ MAXBOUNCES = 5
 MAXTIME = 10
 GRAVITATIONALPULL = 9.81
 REBOUND = 0.8
-BALLRADIUS = 0
-STARTHEIGHT = 2
-TABLEHEIGHT = 0
-STARTVELOCITY = 0
-STEPDISTANCE = 0.1
+BALLRADIUS = 0.0
+STARTHEIGHT = 2.0
+TABLEHEIGHT = 0.0
+STARTVELOCITY = 0.0
+STEPDISTANCE = 0.01
 AIRRESISTANCE = 0.17
 
 # euler Integration function
@@ -28,18 +29,25 @@ def eulerSimulateFallTrajectoryODE():
     # list to save results
     functionResultsApprox = []
     functionResultsApprox.append(STARTHEIGHT)
-    velocity = lambda t : STARTVELOCITY + -GRAVITATIONALPULL * currentTime + (-AIRRESISTANCE * (STARTVELOCITY + -GRAVITATIONALPULL * currentTime))
-
-    # until either the maxtime is reached or either one of the bounds is crossed calculate ODE results using Euler
+    # initiate problem specific function for euler
+    velocity = lambda t : (STARTVELOCITY + -GRAVITATIONALPULL * t) * (1-AIRRESISTANCE)
+    
+    # until either the maxtime is reached or the max number of bounces are reached iterate for new y values
     while totalTime < MAXTIME and numBounces < MAXBOUNCES:
+        # every iteration append a new y value to the results
         functionResultsApprox.append(eulerIntegration(velocity, STEPDISTANCE, functionResultsApprox[-1], currentTime))
 
+        # on bounce
         if functionResultsApprox[-1] < TABLEHEIGHT:
+            # calculate better approximation of impact time
             impactTime = approximateTimeOfImpact(TABLEHEIGHT, velocity, functionResultsApprox[-1], STEPDISTANCE, currentTime, IMPACTPOINTAPPROXIMATIONMAXDEPTH)
+            # update variables on rebound
             currentVelocity = REBOUND * -velocity(currentTime)
             numBounces += 1
             currentTime = 0
-            velocity = lambda t : currentVelocity + - GRAVITATIONALPULL * currentTime + (-AIRRESISTANCE * STARTVELOCITY + -GRAVITATIONALPULL * currentTime)
+            # redefine function to account for updated initial velocity
+            velocity = lambda t : (currentVelocity + -GRAVITATIONALPULL * t) * (1-AIRRESISTANCE)
+            # set last result to more exact value by calculating euler with the remaining time after approximation of impact time
             functionResultsApprox[-1] = eulerIntegration(velocity, impactTime, TABLEHEIGHT, currentTime - STEPDISTANCE + impactTime)
         
         currentTime += STEPDISTANCE
@@ -52,12 +60,15 @@ def approximateTimeOfImpact(target, function, previousValue, stepDistance, curre
     newStepDistance = stepDistance/2
     bestResult = eulerIntegration(function, stepDistance,previousValue,currentTime)
     bestStep = stepDistance
+    # until maxdepth is reached search for value closer to zero
     while currentDepth < maxDepth:
         eulerResult = eulerIntegration(function, newStepDistance, previousValue, currentTime)
+        # save best result and return at the end
         if abs(target - eulerResult) < abs(target - bestResult):
             bestResult = eulerResult
             bestStep = newStepDistance
 
+        # set newStepDistance depending on euler result
         if eulerResult == target:
             return bestStep
         elif eulerResult < target:
@@ -74,10 +85,7 @@ for i in range(0,len(yApprox)):
 
 plt.figure(figsize = (12, 8))
 plt.plot(time, yApprox, 'bo--', label='Approximate')
-#plt.plot(time, y, 'bo--', label='Approximate')
-#plt.plot(time, yExact, 'g', label='Exact')
-plt.title('Approximate and Exact Solution \
-for falling ping pong ball positions')
+plt.title('Approximations for falling ping pong ball positions')
 plt.xlabel('t')
 plt.ylabel('y(t)')
 plt.grid()
